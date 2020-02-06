3d74faa148ed8b24e52c318dc8a62fdd8ec8a791
Business Valley <Business Valley@DESKTOP-TJ3N5CM>
2020-02-06 18:24:49 +0200

    try

--- incalltask/incalltask/Helper/Constants.cs
+++ incalltask/incalltask/Helper/Constants.cs
@@ -9,8 +9,8 @@ namespace incalltask.Helper
         // public static readonly string service_provider_key = "0698a6fd-60f8-4e4f-9804-e10a962f6030";
         //public static readonly string service_provider_key_Api = "http://204.48.21.29:81/auth/serviceprovider/api/informations";
         // this for test 
-        public static readonly string service_provider_key = "d61ff7da-0f7b-4a38-8d56-92cb5ff83e6fa";
-        public static readonly string service_provider_key_Api = "http://api.sip.twasal.co/api/v1/information";
+        public static readonly string service_provider_key = "4e56baf1-087e-4dd9-b80f-2332482054a6";
+        public static readonly string service_provider_key_Api = "http://micros.twasal.co:81/customer/sip/provisioning";
         public static bool UDP_Enabled { get; set; }
         public static bool TLS_Enabled { get; set; }
         public static bool TCP_Enabled { get; set; }
